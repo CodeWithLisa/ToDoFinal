293da3be7f00818389312574854ff44b353c1ba6
CodeWithLisa <83184087+CodeWithLisa@users.noreply.github.com>
2021-05-15 10:42:50 -0400

    Add files via upload

---
+++ To Do/To Do/CompleteToDoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CompleteToDoViewController.swift
+//  To Do
+//
+//  Created by Apple on 5/13/21.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+    var previousVC = TableViewController()
+    var selectedToDo : ToDoCD?
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = selectedToDo?.name
+       }
+        // Do any additional setup after loading the view.
+    
+    
+    @IBAction func completeTapped(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+           if let theToDo = selectedToDo {
+             context.delete(theToDo)
+             navigationController?.popViewController(animated: true)
+           }
+         }
+             }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
---
+++ To Do/To Do/TableViewController.swift
@@ -0,0 +1,96 @@
+//
+//  TableViewController.swift
+//  To Do
+//
+//  Created by Apple on 5/11/21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var toDos : [ToDoCD] = []
+    func getToDos() {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+
+        if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                toDos = coreDataToDos
+                tableView.reloadData()
+        }
+      }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+      getToDos()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    /*func createToDos() -> [ToDo] {
+
+      let swift = ToDo()
+      swift.name = "Learn Swift"
+      swift.important = true
+
+      let dog = ToDo()
+      dog.name = "Walk the Dog"
+      // important is set to false by default
+
+      return [swift, dog]*/
+    
+    // MARK: - Table view data source
+
+  
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+      let toDo = toDos[indexPath.row]
+
+      if let name = toDo.name {
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+      }
+
+      return cell
+    }
+    
+
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
+            if let completeVC = segue.destination as? CompleteToDoViewController {
+                if let toDo = sender as? ToDoCD {
+                  completeVC.selectedToDo = toDo
+                  completeVC.previousVC = self
+                }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+       
+}
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+      // this gives us a single ToDo
+      let toDo = toDos[indexPath.row]
+
+      performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
+
+}
